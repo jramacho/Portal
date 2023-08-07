91c673d699ee726371c2d96f1670c6d9b1c885b6
jramacho <jramacho@maxstoreinformatica.com.br>
2023-08-07 14:04:56 -0300

    rodape

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -7,22 +7,22 @@ export default function Rodape({cliente, email, website, colaborador, conta,
       <footer className="footer border-t-2 border-gray-300 pt-5">
         <ul className="flex flex-wrap items-center justify-center">
           <li>
-            <span className="font-bold">Your name:</span> {colaborador}
+            <span className="font-bold">Vendedor:</span> {colaborador}
           </li>
           <li>
-            <span className="font-bold">Your email:</span> {email}
+            <span className="font-bold">Email:</span> {email}
           </li>
           <li>
-            <span className="font-bold">Phone number:</span> {telefone}
+            <span className="font-bold">Telefone:</span> {telefone}
           </li>
           <li>
-            <span className="font-bold">Bank:</span> {banco}
+            <span className="font-bold">Banco:</span> {banco}
           </li>
           <li>
-            <span className="font-bold">Account holder:</span> {cliente}
+            <span className="font-bold">Cliente:</span> {cliente}
           </li>
           <li>
-            <span className="font-bold">Account number:</span> {conta}
+            <span className="font-bold">Conta:</span> {conta}
           </li>
           <li>
             <span className="font-bold">Website:</span>{" "}
